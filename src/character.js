9ed39e0ab6c42ba521b14967d183a9c95bbff854
Olli Etuaho <olli.etuaho@gmail.com>
2017-04-22 15:20:14 +0300

    Rotate character model according to movement

--- src/character.js
+++ src/character.js
@@ -28,10 +28,14 @@ Character.prototype.initCharacter = function(options) {
 
     // TODO: This is just test geometry / collision mesh. Remove.
     var boxGeometry = new THREE.BoxGeometry(this.size, 2, this.size);
-    var material = Level.dinnerTableMaterial;
-    var box = new THREE.Mesh(boxGeometry, material);
+    var box = new THREE.Mesh(boxGeometry, Level.dinnerTableMaterial);
     box.position.y = 1;
     this.center.add(box);
+    var boxGeometry = new THREE.BoxGeometry(1.0, 0.1, 0.1);
+    var nose = new THREE.Mesh(boxGeometry, Level.dinnerTableMaterial);
+    nose.position.y = 1.5;
+    nose.position.x = 0.5;
+    this.center.add(nose);
     
     this.initThreeSceneObject({
         object: this.center,
@@ -89,16 +93,18 @@ PlayerCharacter.prototype.update = function(deltaTime) {
     this.physicsShim.dx = mathUtil.clamp(-1.0, 1.0, this.xMoveIntent);
     this.physicsShim.dy = mathUtil.clamp(-1.0, 1.0, this.zMoveIntent);
     // Normalize the movement speed: Make sure that diagonal movement speed is the same as horizontal/vertical.
-    if (this.physicsShim.dx != 0.0 && this.physicsShim.dy != 0.0) {
+    if (this.physicsShim.dx != 0.0 || this.physicsShim.dy != 0.0) {
         var movementMult = 1.0 / Math.sqrt(Math.pow(this.physicsShim.dx, 2) + Math.pow(this.physicsShim.dy, 2));
         this.physicsShim.dx *= movementMult;
         this.physicsShim.dy *= movementMult;
+        var displayAngle = Math.atan2(this.physicsShim.dx, this.physicsShim.dy) - Math.PI * 0.5;
+        this.center.rotation.y = displayAngle;
     }
-    this.physicsShim.dx *= Game.parameters.get('playerMoveSpeed')
-    this.physicsShim.dy *= Game.parameters.get('playerMoveSpeed')
+    this.physicsShim.dx *= Game.parameters.get('playerMoveSpeed');
+    this.physicsShim.dy *= Game.parameters.get('playerMoveSpeed');
     GJS.PlatformingPhysics.moveAndCollide(this.physicsShim, deltaTime, 'x', [this.level.physicalCollisionTileMap]);
     GJS.PlatformingPhysics.moveAndCollide(this.physicsShim, deltaTime, 'y', [this.level.physicalCollisionTileMap]);
-    
+
     this.x = this.physicsShim.x;
     this.z = this.physicsShim.y;
     this.center.position.x = this.x;
