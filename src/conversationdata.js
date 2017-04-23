e8994c915e4b272dc1fbc591f56e07a332758b98
Olli Etuaho <olli.etuaho@gmail.com>
2017-04-23 11:06:39 +0300

    Add conversation data

---
+++ src/conversationdata.js
@@ -0,0 +1,97 @@
+'use strict';
+
+var conversationData =
+[
+  {
+    "name": "Weather",
+    "lines": [
+      "I think it's going to rain.",
+      "It's very mild for this time of year.",
+      "What a lovely day!"
+    ]
+  },
+  {
+    "name": "Gardening",
+    "lines": [
+      "Their roses are the best in the village.",
+      "Yes, but do you prune it?",
+      "It's all about the right kind of manure."
+    ]
+  },
+  {
+    "name": "Art",
+    "lines": [
+      "I find his work rather pretentious.",
+      "It transcends beauty, I tell you!",
+      "It's... interesting."
+    ]
+  },
+  {
+    "name": "Travel",
+    "lines": [
+      "You must go! We did and we loved it.",
+      "The flight was dreadful.",
+      "Travel is food for the soul, dontcha know."
+    ]
+  },
+  {
+    "name": "Politics",
+    "lines": [
+      "I'm thinking of going into politics myself.",
+      "The two state solution is really the...",
+      "I would not vote for those crooks even!"
+    ]
+  },
+  {
+    "name": "Village gossip",
+    "lines": [
+      "And so she married the vicar.",
+      "I don't think that's a good place to hide a body.",
+      "Well, we all knew that!"
+    ]
+  },
+  {
+    "name": "Religion",
+    "lines": [
+      "Charity, always charity!",
+      "We need to spread the good word.",
+      "That's not what it says in the... "
+    ]
+  },
+  {
+    "name": "Cars",
+    "lines": [
+      "30 miles to the gallon.",
+      "Took it to the mechanic the other day...",
+      "The slick lines, the leather seats - heaven!"
+    ]
+  },
+  {
+    "name": "Health",
+    "lines": [
+      "Two liters of water every day, at least.",
+      "A very strange pain right here.",
+      "The doctor said..."
+    ]
+  },
+  {
+    "name": "Pop culture",
+    "lines": [
+      "Who's that guy in that thing?"
+    ]
+  },
+  {
+    "name": "Work",
+    "lines": [
+      "My boss is such a numpty.",
+      "I do it for the paychecks.",
+      "It's a living, yeh?"
+    ]
+  },
+  {
+    "name": "Sports",
+    "lines": [
+      "Did you catch the game last night?"
+    ]
+  }
+];
